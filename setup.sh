1b622abacdd27b79bc1414692883d86d4182137a
Sam Boling <charles.samuel.boling@gmail.com>
2019-10-05 09:53:00 -0600

    clean up poetry setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 set -ex
 curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
 poetry install
-
-# python -m venv .
-# pip install -r requirements.txt
-# echo "ready. run:"
-# echo "  source Scripts/activate"
-# echo "in this folder before running druid when you've opened a new shell"
+set +x
+echo "see you back here after you've added poetry to your path and restarted your shell"
+echo "then: poetry run python druid.py"
