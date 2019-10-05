f2239b754d6df72e36b52331b94d26805d442dff
Sam Boling <charles.samuel.boling@gmail.com>
2019-10-05 09:51:09 -0600

    attempt to use poetry for setup instead

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 set -ex
-python -m venv .
-pip install -r requirements.txt
-echo "ready. run:"
-echo "  source Scripts/activate"
-echo "in this folder before running druid when you've opened a new shell"
+curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
+poetry install
+
+# python -m venv .
+# pip install -r requirements.txt
+# echo "ready. run:"
+# echo "  source Scripts/activate"
+# echo "in this folder before running druid when you've opened a new shell"
